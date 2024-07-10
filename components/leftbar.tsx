4dd763bdc35b176c222c0e64387a76c9b46c5145
Paul Tuck <paul.tuck@oxon.org>
2024-07-10 20:01:55 +0700

    remove eslint and add biomejs

--- components/leftbar.tsx
+++ components/leftbar.tsx
@@ -17,12 +17,12 @@ export function Leftbar() {
 	return (
 		<aside className="md:flex hidden flex-[0.9] min-w-[230px] sticky top-16 flex-col h-[92.75vh] overflow-y-auto">
 			<ScrollArea className="py-4">
-				{ROUTES.map(({ href, items, title }: any) => {
+				{ROUTES.map(({ title, href, chapters }: any) => {
 					return (
 						<div className="flex flex-col gap-3 mt-5" key={href}>
 							<h4 className="font-medium text-sm">{title}</h4>
 							<div className="flex flex-col gap-3 text-sm dark:text-neutral-300/85 text-neutral-800 ml-0.5">
-								{items.map((subItem: any) => {
+								{chapters.map((subItem: any) => {
 									const key = `/docs/${href}${subItem.href}`;
 									return (
 										<Anchor
